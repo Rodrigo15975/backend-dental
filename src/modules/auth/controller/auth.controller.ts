9c3148d8ae5bd751c2ed8f0a2c7566999ef8c42b
BraunRumpler <javierluis15975@hotmail.com>
2024-06-28 02:06:30 -0500

    dental

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,17 +1,32 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthUserGuard } from '../guards/auth-guards';
 import { AuthService } from '../services/auth.service';
 import { AuthData } from '../types/type-auth';
-import { AuthUserGuard } from '../guards/auth-guards';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() data: AuthData) {
+  async login(
+    @Body() data: AuthData,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const auth = await this.authService.signIn(data);
-    return { auth };
+    res.cookie('auth', auth, {
+      sameSite: 'none',
+      secure: true,
+    });
+    res.send({ auth });
   }
 
   @UseGuards(AuthUserGuard)
