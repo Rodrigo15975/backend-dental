520ec053f040205f6e535408ee4aae2306e3e2e2
BraunRumpler <javierluis15975@hotmail.com>
2024-06-28 00:21:34 -0500

    dental-backend

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from '../services/auth.service';
 import { AuthData } from '../types/type-auth';
-import { AuthUserGuard } from '../guards/auth-guards';
 
 @Controller('auth')
 export class AuthController {
@@ -14,7 +13,7 @@ export class AuthController {
     return { auth };
   }
 
-  @UseGuards(AuthUserGuard)
+  // @UseGuards(AuthUserGuard)
   @Get('verify')
   tokenSuccess() {
     return {
@@ -23,13 +22,13 @@ export class AuthController {
       message: 'Verification successful!',
     };
   }
-  @UseGuards(AuthUserGuard)
+  // @UseGuards(AuthUserGuard)
   @Get('profile')
   getId(@Req() req: Request) {
     return req.user;
   }
 
-  @UseGuards(AuthUserGuard)
+  // @UseGuards(AuthUserGuard)
   @Get('logout')
   logout() {
     return { message: 'Sesión cerrada exitosa' };
